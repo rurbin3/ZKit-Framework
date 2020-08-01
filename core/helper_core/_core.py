d62ca0187c63e82ac158cf9272acc678cf0ab728
Zer0 <65390948+000Zer000@users.noreply.github.com>
2020-08-01 16:49:55 +0800

    Update _core.py

--- core/helper_core/_core.py
+++ core/helper_core/_core.py
@@ -5,34 +5,20 @@ The Core Module of ZKit-Framework Contains : some useful methods like a custom c
 # Python 2 is supported too
 from __future__ import print_function, division, absolute_import
 import os
-
-
-def Check():
-    if os.name == 'nt':
-        try:
-            import colorama
-        except (ImportError, ModuleNotFoundError):
-            raise SystemExit(
-                "Colorama Not Found . Install It Via Pip (pip install colorama)")
-
-    try:
-        import scapy
-    except(ImportError, ModuleNotFoundError):
-        raise SystemExit(
-            "Scapy Not Found . Install It Via Pip (pip install scapy)")
-    return True
-
-
+import random
+from base64 import b85encode as be
+from core.helper_core._banners import banner1, banner2
+from core.helper_core import get_rootkit
 def init():
-
-    PATH = os.path.dirname(os.path.dirname(os.path.dirname(__file__)))
-    rootkit_path = PATH + "\\Builded\\Rootkit\\"
-    keylogger_path = PATH + "\\Builded\\KeyLogger\\"
-    ransomware_path = PATH + "\\Builded\\Ransomware\\"
+    'init the zkit . without you will get several errors'
+    path_ = os.path.dirname(os.path.dirname(os.path.dirname(__file__)))
+    rootkit_path = path_ + "\\Builded\\Rootkit\\"
+    keylogger_path = path_ + "\\Builded\\KeyLogger\\"
+    ransomware_path = path_ + "\\Builded\\Ransomware\\"
     os.popen("mkdir {}".format(rootkit_path)).close()
     os.popen("mkdir {}".format(keylogger_path)).close()
     os.popen("mkdir {}".format(ransomware_path)).close()
-    os.popen("mkdir {}".format(PATH + '\\Loot')).close()
+    os.popen("mkdir {}".format(path_ + '\\Loot')).close()
     if os.name == "nt":
         os.system("cls")
     else:
@@ -40,6 +26,7 @@ def init():
 
 
 class Color:
+    'ANSI Color codes'
     def __init__(self):
         self.colors = {
             "black": "\033[30m",
@@ -53,10 +40,12 @@ class Color:
             "reset": "\033[0m",
         }
 
-    def GetColor(self, colorname):
-        return self.colors.get(colorname)
+    def GetColor(self, colorname): # pylint: disable=C0103; # noqa
+        'Gets a color from colors dict case insensetive'
+        return self.colors.get(colorname.lower())
 
-    def GetAllColors(self):
+    def GetAllColors(self): # pylint: disable=C0103; # noqa
+        "returns all colors and reset as a tuple"
         return tuple(self.colors.values())
 
 
@@ -66,29 +55,30 @@ def notify(status: str, message: str, ending="\n", flush=False):
 
     Args:
         message (str): message shown for the user
-        status (str): status for event . all possibleties are "notify", "problem", "report" and "question"
+        status (str): status for event . all possibleties are "notify",
+        "problem", "report" and "question"
         statuses will be converted to lower .
         ending (str): used as value for print(message, end = ending)
         flush (bool): to flush the stream or not (default=False)
     """
-    c = Color()
-    reset = c.GetColor('reset')
+    col = Color()
+    reset = col.GetColor('reset')
     status = status.lower()
-    all_status = {'report': 'blue|[ REPORT ]',
-                  'notify': 'green|[ NOTIFY ]',
-                  'problem': 'red|[CRITICAL]',
-                  'question': 'yellow|[QUESTION]',
-                  }
-    for st in all_status:
-        if st == status:
-            temp = all_status[st].split('|')
-            first = c.GetColor(temp[0]) + temp[1] + reset
+    all_stats = {'report': 'blue|[ REPORT ]',
+                 'notify': 'green|[ NOTIFY ]',
+                 'problem': 'red|[CRITICAL]',
+                 'question': 'yellow|[QUESTION]',
+                }
+    for stat in all_stats:
+        if stat == status:
+            temp = all_stats[stat].split('|')
+            first = col.GetColor(temp[0]) + temp[1] + reset
             break
     print("{}{} {}".format(
-          '\r' if flush else '', first, message), end=ending)
+        '\r' if flush else '', first, message), end=ending)
 
 
-def ask_for(message: str, report: str, default=['', ''], type=str, args=()):
+def ask_for(message: str, report: str, default=None, type=str, args=()): # pylint: disable=W0622; # noqa
     """
     Asks for anything from users . (uses notify)
 
@@ -101,7 +91,8 @@ def ask_for(message: str, report: str, default=['', ''], type=str, args=()):
         args (tuple): [description]. Defaults to ().
 
     Returns:
-        (type): the value user entered in type of the "type" argument. changed if matched the default to whatever to put in defaults[1]
+        (type): the value user entered in type of the "type" argument.
+        changed if matched the default to whatever to put in defaults[1]
     """
     notify('question', message, '')
     if type != list:
@@ -136,10 +127,11 @@ def create_file(file: str):
     """
     notify("notify", "Creating File...", "")
     try:
-        _ = open(file,  "x").close()
+        _ = open(file, "x").close()
     except FileExistsError:
         notify(
-            "problem", "Creating File...Failed \nFile Already Exists Confirm Overwrite : (N or Y) ", "", True
+            "problem", "Creating File...Failed \n" +
+            "File Already Exists Confirm Overwrite : (N or Y) ", "", True
         )
         choice = str(input("")).upper()
         if choice == "Y":
@@ -151,11 +143,11 @@ def create_file(file: str):
             while True:
                 notify("File Already Exists Confirm Overwrite : (N or Y) ", "")
                 choice = str(input("")).upper()
-                if choice == "Y":
+                if choice == "Y": # pylint: disable=R1723; # noqa
                     break
                 elif choice == "N":
                     file = str(
-                        input("Write Down File Name Here : ")) + ".pyw"
+                        input("Write Down An Other File Name Here : ")) + ".pyw"
                     break
                 else:
                     notify("problem", "\nInvalid Input Try Again")
@@ -166,14 +158,13 @@ def create_file(file: str):
 
 
 def generate(payload_type):
-    from base64 import b85encode as be, b85decode as bd
-    from core.helper_core import get_rootkit
+    "generates payloads with given payload_type"
     if payload_type == "rootkit":
         payload, path = get_rootkit()
     notify("notify", "Opening File To Write Data On It...", "")
     try:
-        f = open(path, "w+")
-    except:
+        file = open(path, "w+")
+    except Exception: # pylint: disable=W0703; # noqa
         notify(
             "problem", "Opening File To Write Data On It...Failed \n Cannnot Open File", "\n", True)
         return
@@ -188,15 +179,16 @@ def generate(payload_type):
         payload + '"""\nexec(b85decode(value))'
     notify("notify", "Writing Data On File...", "")
     try:
-        f.write(payload)
+        file.write(payload)
     except PermissionError:
         notify("problem", "Writing Data On File...Failed \nSomething Went Wrong . Looks Like "
                "You Dont Have Access To The File." "\n", True)
-    except:
-        notify("problem", "Writing Data On File...Failed \nSomething Went Wrong . Is Another Process "
+    except Exception: # pylint: disable=W0703; # noqa
+        notify("problem", "Writing Data On File...Failed \nSomething Went Wrong . " +
+               "Is Another Process "
                "Using It ? ", "\n", True)
         notify("problem", "Couldnt Write Data On File Closing File...", "")
-        f.close()
+        file.close()
         print("Done")
     else:
         notify("report",
@@ -206,9 +198,8 @@ def generate(payload_type):
 
 
 def print_banner():
-    from core.helper_core._banners import banner1, banner2
-    import random
-    black, red, green, yellow, blue, magenta, cyan, grey, reset = Color().GetAllColors()
+    "gets a random color and a random banner and prints it"
+    _, red, green, _, blue, magenta, cyan, _, reset = Color().GetAllColors()
     random.seed(random.choice(
         [random.randint(1, 9999), random.randint(1, 998)]))
     banner = random.choice([banner1, banner2])
@@ -218,5 +209,3 @@ def print_banner():
 
 if __name__ == "__main__":
     print("Dont Run This File")
-# Used For Test
-# dos_ss("192.168.1.1", "127.0.0.1", 110 , 80 , 1, "Test")
