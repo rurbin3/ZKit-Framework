9708586c64fb45b122aa4ed5dbac0c9edff42c53
Zer0 <65390948+000Zer000@users.noreply.github.com>
2020-08-01 17:44:19 +0800

    increased readability in zkit.py

--- zkit.py
+++ zkit.py
@@ -3,39 +3,40 @@
 # This Work Is Licensed Under Apache Software License 2.0 More
 # Can Be Found In The LICENSE File.
 __author__ = "Zer0"
-__github__ = "https://github.com/000Zer000/ZKit-Framework"
 __version__ = "1.3.5"
 __license__ = "Apache Software License 2.0"
 __status__ = "Production"
 
+try:
+    # Doing some imports
+    from core.helper_core import notify, Color, generate, dos, \
+        ctrler, helpbanner, init, print_banner
+except (ImportError, ModuleNotFoundError) as value:
+    # Ops ! Sth is missing
+
+    print(
+        "One Or Some On Requirments Not Found . Please Install Them And Try Again ."
+        + "Python Threw : "
+        + str(value)
+    )
+    raise
+
+
 def start():
-    try:
-        # Doing some imports
-        from os import popen, path, name as os_name, system
-        import core.helper_core
-        from core.helper_core import notify, Color, generate, dos, ctrler, helpbanner, init, print_banner
-    except (ImportError,ModuleNotFoundError) as value:
-        # Ops ! Sth is missing
-
-        print(
-            "One Or Some On Requirments Not Found . Please Install Them And Try Again ."
-            + "Python Threw : "
-            + str(value)
-        )
-        raise 
-
+    "Starts zkit with those beautiful menues"
     # Printing A Banner More Coming Soon
-    black, red, green, yellow, blue, magenta, cyan, grey, reset = Color().GetAllColors()
+    _, red, green, yellow, blue, magenta, cyan, _, reset = Color().GetAllColors()
     init()
     print_banner()
     # Hard And Boring Code
     print(
-        "\t \t \t \t \t    Hacking is" + red + " C " + green + "O " + blue + "L " +
+        "\t " * 5 + "Hacking is" + red + " C " + green + "O " + blue + "L " +
         yellow + "O " + magenta + "R " + green +
         "F " + red + "U " + magenta + "L " + reset
     )
     print(
-        "Available Options Are or Enter '?' To get a summery about notes of using this framework:\n" "* = Not Supported right now\n"
+        "Available Options Are or Enter '?' To get a summery about notes of using this framework:\n"
+        + "* = Not Supported right now\n"
         + red + "  {1} --> Create A RootKit\n"
         + green + "  {2} --> Create A Ransomware *\n"
         + blue + "  {3} --> Create A KeyLogger *\n"
@@ -45,25 +46,26 @@ def start():
     )
     while True:
         try:
-            CHOICE = str(input("..> "))
+            choice = str(input("..> "))
 
-            if CHOICE == "000":
+            if choice == "000":
                 break
-            elif CHOICE == "?":
+            if choice == "?":
                 print(helpbanner)
-            elif CHOICE == "1":
+            elif choice == "1":
                 generate("rootkit")
-            elif CHOICE == "4":
+            elif choice == "4":
                 dos.main()
-            elif CHOICE == "5":
+            elif choice == "5":
                 ctrler.main()
-            elif CHOICE != None:
+            elif choice is not None:
                 notify(
-                    "problem", "Invalid Input {" + "{}".format(CHOICE) + "}")
+                    "problem", "Invalid Input {" + "{}".format(choice) + "}")
         except (KeyboardInterrupt, EOFError):
-            try :
+            try:
                 print("\nPlease Type '000' To Exit ZKit-Framework\n")
-                CHOICE = None
+                choice = None
             except(KeyboardInterrupt, EOFError):
                 pass
+
 start()
